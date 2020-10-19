3faea959308c5eb76db233a278d2c058b13f7b43
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-19 15:14:24 +0200

    added main projects

---
+++ TradeAnalytics.Service/Infrastructure/DataBase/UpsLightContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+using TradeAnalitycs.Service.Models;
+
+namespace TradeAnalitycs.Service
+{
+    public class UpsLightContext : DbContext
+    {
+        public DbSet<ProductTrade> ProductTrade { get; set; }
+
+        protected UpsLightContext() { }
+        public UpsLightContext (DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ TradeAnalytics.Service/Model/ProductTrade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TradeAnalitycs.Service.Models
+{
+    public class ProductTrade
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        public DateTime Date { get; set; }
+
+        public Guid ProductId { get; set; }
+
+        public string Details { get; set; }
+
+        public Guid SchemaId { get; set; }
+
+        public Guid TradeId { get; set; }
+
+        public Guid PortfolioId { get; set; }
+
+        public ProductTrade(DateTime date, Guid productId, string details, Guid schemaId, Guid tradeId, Guid portfolioId)
+        {
+            Id = new Guid();
+            Date = date;
+            ProductId = productId;
+            Details = details;
+            SchemaId = schemaId;
+            TradeId = schemaId;
+            PortfolioId = portfolioId;
+        }
+
+    }
+}
